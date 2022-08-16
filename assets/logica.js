cc870e5956d6ea5939c3f683e45d254b53fc167d
Ricardo <riicardo.ro@gmail.com>
2022-08-16 02:29:08 -0500

    Se crea el repo y se sube logica y diseño primer propuesta

---
+++ assets/logica.js
@@ -0,0 +1,53 @@
+const txtEntrada = document.getElementById('txtEntrada');
+const txtSalida = document.getElementById('txtSalida');
+const btnEncriptar = document.getElementById('btnEncriptar'); 
+const btnDesencriptar = document.getElementById('btnDesencriptar');
+
+btnEncriptar.addEventListener('click', (e) =>{
+    if(txtEntrada.value != ''){
+
+        document.getElementById('sinMensaje').style.display = "none";
+        txtSalida.style.display = "block";
+
+        const arr = document.getElementById('txtEntrada').value.split('')
+        for (i=0; i<arr.length; i++){
+            switch(arr[i]){
+                case "e": arr[i] = 'enter';
+                    break;
+                case "i": arr[i] = 'imes';
+                    break;
+                case "a": arr[i] = 'ai';
+                    break;
+                case "o": arr[i] = 'ober';
+                    break;
+                case "u": arr[i] = 'ufat';
+                    break;
+            }
+        }
+        txtSalida.innerText = arr.join('')
+        txtEntrada.innerText = '';
+
+    } else{
+
+        document.getElementById('sinMensaje').style.display = "block";
+        txtSalida.style.display = "none";         
+    }
+}
+)
+
+function desencriptar(){
+    let txtDesencriptado = document.getElementById('txtEntrada').value
+    txtDesencriptado = txtDesencriptado.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+    txtSalida.value = txtDesencriptado
+}
+
+function copyToClipBoard(txtSalida) {
+    //copia de textarea
+    txtSalida = document.getElementById('txtSalida');
+    txtSalida.select();
+    document.execCommand('copy'); 
+}
+
+function borrar(){
+    txtSalida.value = '';
+}
